f40b80a59bfc463e55314f421e6c6c5ab693f00d
Vi Đăng Khoa <75578740+khoavd1102@users.noreply.github.com>
2022-11-05 11:38:16 +0700

    cs _layout

--- app/src/main/java/com/example/calculator/Calculator.java
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Calculator extends AppCompatActivity {
 
